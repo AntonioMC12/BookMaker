677a64b5b67f633136e68acb4d4230f0087c2873
ErTonix12 <tonicubero12@gmail.com>
2021-12-02 14:00:01 +0100

    Issue #5 Creación de Interfaces

---
+++ src/main/java/es/iesfranciscodelosrios/BookMaker/model/IDAO/IGlobalNoteDAO.java
@@ -0,0 +1,7 @@
+package es.iesfranciscodelosrios.BookMaker.model.IDAO;
+
+import es.iesfranciscodelosrios.BookMaker.model.IDO.IGlobalNote;
+
+public interface IGlobalNoteDAO extends IDAO<IGlobalNote, Long>{
+
+}
