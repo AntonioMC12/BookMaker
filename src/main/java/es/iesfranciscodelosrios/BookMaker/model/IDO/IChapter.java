0d99d3390d5181947dd0bed7b4601f10c08e4d8e
nicorrt <nicorrt123@gmail.com>
2021-12-03 10:19:13 +0100

    Issue #10 Chapter and ChapterNote Created

--- src/main/java/es/iesfranciscodelosrios/BookMaker/model/IDO/IChapter.java
+++ src/main/java/es/iesfranciscodelosrios/BookMaker/model/IDO/IChapter.java
@@ -1,16 +1,20 @@
 package es.iesfranciscodelosrios.BookMaker.model.IDO;
 
+import java.util.List;
+
 public interface IChapter {
 
 	public void setId(Long id);
 	public void setAct(IAct act);
 	public void setName(String name);
 	public void setText(String text);
-
+	public void setNotesChapter(List<IChapterNote> notesChapter);
 
 	
 	public Long getId();
 	public IAct getAct();
 	public String getName();
 	public String getText();
+	public List<IChapterNote> getNotesChapter();
+
 }
