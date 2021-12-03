f2ed664920787be7a73410a2ac2f0a9710dac4d2
ErTonix12 <tonicubero12@gmail.com>
2021-12-03 09:20:43 +0100

    modulo requires

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module es.iesfranciscodelosrios.BookMaker {
     requires javafx.controls;
     requires javafx.fxml;
-//    requires java.persistence;
+    requires java.persistence;
 
     opens es.iesfranciscodelosrios.BookMaker to javafx.fxml;
     exports es.iesfranciscodelosrios.BookMaker;
