c8efe6fde9552f28328f5f5dacb26d596b3b0e47
ErTonix12 <tonicubero12@gmail.com>
2021-12-10 12:58:42 +0100

    edited PersistenceUnit

--- src/main/java/es/iesfranciscodelosrios/BookMaker/utils/PersistenceUnit.java
+++ src/main/java/es/iesfranciscodelosrios/BookMaker/utils/PersistenceUnit.java
@@ -6,18 +6,40 @@ import javax.persistence.Persistence;
 public class PersistenceUnit {
 
 	private static EntityManagerFactory emf;
-	private static final String PUN = "aplicacionMariaDB";
+	public static boolean mode = false; //false -> local & true -> remote
+	private static final String LOCAL = "aplicacionH2";
+	private static final String REMOTE = "aplicacionMariaDB";
 
 	public static EntityManagerFactory getInstance(String name) {
 		if (emf == null) {
-			emf = Persistence.createEntityManagerFactory(name);
+			if (name.equals(LOCAL) || name.equals(REMOTE)) {
+				emf = Persistence.createEntityManagerFactory(name);
+			}
 		}
 		return emf;
 	}
 
+//	public static EntityManagerFactory getRemoteInstance() {
+//		if (emf == null) {
+//			emf = Persistence.createEntityManagerFactory(REMOTE);
+//		}
+//		return emf;
+//	}
+//
+//	public static EntityManagerFactory getLocalInstance() {
+//		if (emf == null) {
+//			emf = Persistence.createEntityManagerFactory(LOCAL);
+//		}
+//		return emf;
+//	}
+	
 	public static EntityManagerFactory getInstance() {
 		if (emf == null) {
-			emf = Persistence.createEntityManagerFactory(PUN);
+			if(mode) {
+				emf = Persistence.createEntityManagerFactory(REMOTE);
+			}else {
+				emf = Persistence.createEntityManagerFactory(LOCAL);
+			}
 		}
 		return emf;
 	}
