600bb9fe642d7fcec4ada13151426c94ff7aa53f
ErTonix12 <tonicubero12@gmail.com>
2021-12-10 09:21:52 +0100

    added catch clausules

--- src/main/java/es/iesfranciscodelosrios/BookMaker/model/DAO/BookDAO.java
+++ src/main/java/es/iesfranciscodelosrios/BookMaker/model/DAO/BookDAO.java
@@ -1,16 +1,19 @@
 package es.iesfranciscodelosrios.BookMaker.model.DAO;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TransactionRequiredException;
 
 import es.iesfranciscodelosrios.BookMaker.model.DO.Book;
 import es.iesfranciscodelosrios.BookMaker.model.IDAO.IBookDAO;
 import es.iesfranciscodelosrios.BookMaker.utils.PersistenceUnit;
 
 public class BookDAO implements IBookDAO {
-	
+
 	public static EntityManager createEM() {
 		EntityManagerFactory emf = PersistenceUnit.getInstance();
 		return emf.createEntityManager();
@@ -18,11 +21,23 @@ public class BookDAO implements IBookDAO {
 
 	@Override
 	public void save(Book a) throws DAOException {
-		EntityManager em = createEM();
-		em.getTransaction().begin();
-		em.persist(a);
-		em.getTransaction().commit();
-		
+		try {
+			EntityManager em = createEM();
+			em.getTransaction().begin();
+			em.persist(a);
+			em.getTransaction().commit();
+		} catch (IllegalStateException e) {
+			throw new DAOException("Signals that a method has been invoked at an illegal orinappropriate time.", e);
+		} catch (EntityExistsException e) {
+			throw new DAOException("The entity already exists. ", e);
+		} catch (IllegalArgumentException e) {
+			throw new DAOException("A method has been passed an illegal orinappropriate argument.", e);
+		} catch (TransactionRequiredException e) {
+			throw new DAOException("Transaction is required but is notactive.", e);
+		} catch (Exception e) {
+			throw new DAOException("An error has ocurred", e);
+		}
+
 	}
 
 	@Override
@@ -32,26 +47,64 @@ public class BookDAO implements IBookDAO {
 
 	@Override
 	public void delete(Book a) throws DAOException {
-		EntityManager em = createEM();
-		em.getTransaction().begin();
-		em.remove(a);
-		em.getTransaction().commit();
-		
+		try {
+			EntityManager em = createEM();
+			em.getTransaction().begin();
+			em.remove(a);
+			em.getTransaction().commit();
+		} catch (IllegalStateException e) {
+			throw new DAOException("Signals that a method has been invoked at an illegal orinappropriate time.", e);
+		} catch (EntityExistsException e) {
+			throw new DAOException("The entity already exists. ", e);
+		} catch (IllegalArgumentException e) {
+			throw new DAOException("A method has been passed an illegal orinappropriate argument.", e);
+		} catch (TransactionRequiredException e) {
+			throw new DAOException("Transaction is required but is notactive.", e);
+		} catch (Exception e) {
+			throw new DAOException("An error has ocurred", e);
+		}
+
 	}
 
 	@Override
 	public List<Book> showAll() throws DAOException {
-		EntityManager em = createEM();
-		return em.createQuery("SELECT a FROM Book a", Book.class).getResultList();
+		List<Book> books = new ArrayList<>();
+		try {
+			EntityManager em = createEM();
+			books = em.createQuery("SELECT a FROM Book a", Book.class).getResultList();
+		} catch (IllegalStateException e) {
+			throw new DAOException("Signals that a method has been invoked at an illegal orinappropriate time.", e);
+		} catch (EntityExistsException e) {
+			throw new DAOException("The entity already exists. ", e);
+		} catch (IllegalArgumentException e) {
+			throw new DAOException("A method has been passed an illegal orinappropriate argument.", e);
+		} catch (TransactionRequiredException e) {
+			throw new DAOException("Transaction is required but is notactive.", e);
+		} catch (Exception e) {
+			throw new DAOException("An error has ocurred", e);
+		}
+		return books;
 	}
 
 	@Override
 	public Book show(Long id) throws DAOException {
 		Book result = null;
-		EntityManager em = createEM();
-		em.getTransaction().begin();
-		result = em.find(Book.class, id);
-		em.getTransaction().commit();
+		try {
+			EntityManager em = createEM();
+			em.getTransaction().begin();
+			result = em.find(Book.class, id);
+			em.getTransaction().commit();
+		} catch (IllegalStateException e) {
+			throw new DAOException("Signals that a method has been invoked at an illegal orinappropriate time.", e);
+		} catch (EntityExistsException e) {
+			throw new DAOException("The entity already exists. ", e);
+		} catch (IllegalArgumentException e) {
+			throw new DAOException("A method has been passed an illegal orinappropriate argument.", e);
+		} catch (TransactionRequiredException e) {
+			throw new DAOException("Transaction is required but is notactive.", e);
+		} catch (Exception e) {
+			throw new DAOException("An error has ocurred", e);
+		}
 		return result;
 	}
 
